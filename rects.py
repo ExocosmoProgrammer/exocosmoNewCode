a2a76cc57fb0cefee16a3d16edca4cc44d562201
ExocosmoProgrammer <gooberpea1024@gmail.com>
2023-10-29 08:02:08 -0700

    Update rects.py

--- rects.py
+++ rects.py
@@ -1,11 +1,12 @@
 from definitions import rotate, lesser, greater, checkLineCollision
 from lines import line
-
+from variables import display
+import pygame
 # I will use the rect class for collision detection, so that collision can be checked with rotated rectangles.
 
 
 class rect:
-    def __init__(self, pyRect=None, angle=0, **extra):
+    def __init__(self, pyRect=None, angle=0, pointOfRotation=None, **extra):
         if pyRect != None:
             self.points = [[pyRect.left, pyRect.top], [pyRect.right, pyRect.top],
                            [pyRect.right, pyRect.bottom], [pyRect.left, pyRect.bottom]]
@@ -13,8 +14,14 @@ class rect:
             self.angle = angle
 
             if angle != 0:
-                for i in range(4):
-                    self.points[i] = rotate(self.points[i], self.center, angle)
+                if pointOfRotation is not None:
+                    for i in range(4):
+                        self.points[i] = rotate(self.points[i], eval(pointOfRotation), angle)
+                else:
+                    for i in range(4):
+                        self.points[i] = rotate(self.points[i], self.center, angle)
+
+        self.updatePoints()
 
         for stat in list(extra.keys()):
             exec(f'self.{stat} = extra[stat]')
@@ -69,6 +76,12 @@ class rect:
         self.width = self.right - self.left
         self.height = self.bottom - self.top
 
+    def getEnds(self):
+        self.left = min([point[0] for point in self.points])
+        self.right = max([point[0] for point in self.points])
+        self.bottom = max([point[1] for point in self.points])
+        self.top = min([point[1] for point in self.points])
+
     def getLines(self):
         self.lines = []
 
@@ -85,9 +98,17 @@ class rect:
 
             self.lines.append(line(slope, constant, boundaries))
 
+    def showCollision(self, rectangle):
+
+        for i in self.points:
+            display.fill((255, 255, 255), pygame.Rect(i[0] - 5, i[1] - 5, 10, 10))
+
+        for i in rectangle.points:
+            display.fill((100, 100, 100), pygame.Rect(i[0] - 5, i[1] - 5, 10, 10))
+
     def checkCollision(self, rectangle=None):
-        self.getMajorInfo()
-        rectangle.getMajorInfo()
+        self.updatePoints()
+        rectangle.updatePoints()
 
         if self.points[0][0] == self.points[1][0] or self.points[0][1] == self.points[1][1]:
             selfDiagonal = 0
@@ -101,7 +122,8 @@ class rect:
         else:
             rectDiagonal = 1
 
-        if self.top < rectangle.bottom and rectangle.top < self.bottom and self.left < rectangle.right and rectangle.left < self.right:
+        if self.top < rectangle.bottom and rectangle.top < self.bottom and self.left < rectangle.right and \
+                rectangle.left < self.right:
             pass
 
         else:
@@ -111,16 +133,18 @@ class rect:
             return 1
 
         elif selfDiagonal == 0 and rectDiagonal == 1:
-            for point in rectangle.points:
-                if abs(point[0] - self.centerx) < self.width / 2 and abs(point[1] - self.centery) < self.height / 2:
-                    return 1
-
             rectangle.getLines()
 
-            for point in self.points:
-                if lesser(rectangle.lines[0].YAtX(point[0]), rectangle.lines[2].YAtX(point[0])) <= point[1] <= greater(rectangle.lines[0].YAtX(point[0]), rectangle.lines[2].YAtX(point[0])) \
-                        and lesser(rectangle.lines[1].YAtX(point[0]), rectangle.lines[3].YAtX(point[0])) <= point[1] <= greater(rectangle.lines[1].YAtX(point[0]), rectangle.lines[3].YAtX(point[0])):
-                    return 1
+            try:
+                for point in self.points:
+                    if lesser(rectangle.lines[0].YAtX(point[0]), rectangle.lines[2].YAtX(point[0])) <= point[1] <= \
+                            greater(rectangle.lines[0].YAtX(point[0]), rectangle.lines[2].YAtX(point[0])) \
+                            and lesser(rectangle.lines[1].YAtX(point[0]), rectangle.lines[3].YAtX(point[0])) <= \
+                            point[1] <= greater(rectangle.lines[1].YAtX(point[0]), rectangle.lines[3].YAtX(point[0])):
+                        return 1
+
+            except TypeError:
+                return 1
 
             self.getLines()
 
@@ -130,36 +154,42 @@ class rect:
                         return 1
 
         elif rectDiagonal == 0 and selfDiagonal == 1:
-            for point in self.points:
-                if abs(point[0] - rectangle.centerx) < rectangle.width / 2 \
-                        and abs(point[1] - rectangle.centery) < rectangle.height / 2:
-                    return 1
-
             self.getLines()
 
-            for point in rectangle.points:
-                if lesser(self.lines[0].YAtX(point[0]), self.lines[2].YAtX(point[0])) <= point[1] <= greater(self.lines[0].YAtX(point[0]), self.lines[2].YAtX(point[0])) \
-                        and lesser(self.lines[1].YAtX(point[0]), self.lines[3].YAtX(point[0])) <= point[1] <= greater(self.lines[1].YAtX(point[0]), self.lines[3].YAtX(point[0])):
-                    return 1
+            try:
+                for point in rectangle.points:
+                    if lesser(self.lines[0].YAtX(point[0]), self.lines[2].YAtX(point[0])) <= point[1] <= \
+                            greater(self.lines[0].YAtX(point[0]), self.lines[2].YAtX(point[0])) \
+                            and lesser(self.lines[1].YAtX(point[0]), self.lines[3].YAtX(point[0])) <= point[1] <= \
+                            greater(self.lines[1].YAtX(point[0]), self.lines[3].YAtX(point[0])):
+                        self.showCollision(rectangle)
+                        return 1
+            except TypeError:
+                return 1
 
             rectangle.getLines()
 
             for rectLine in rectangle.lines:
                 for selfLine in self.lines:
                     if checkLineCollision(selfLine, rectLine):
+                        self.showCollision(rectangle)
                         return 1
         else:
             self.getLines()
             rectangle.getLines()
 
             for point in rectangle.points:
-                if lesser(self.lines[0].YAtX(point[0]), self.lines[2].YAtX(point[0])) <= point[1] <= greater(self.lines[0].YAtX(point[0]), self.lines[2].YAtX(point[0])) \
-                        and lesser(self.lines[1].YAtX(point[0]), self.lines[3].YAtX(point[0])) <= point[1] <= greater(self.lines[1].YAtX(point[0]), self.lines[3].YAtX(point[0])):
+                if lesser(self.lines[0].YAtX(point[0]), self.lines[2].YAtX(point[0])) <= point[1] <= \
+                        greater(self.lines[0].YAtX(point[0]), self.lines[2].YAtX(point[0])) \
+                        and lesser(self.lines[1].YAtX(point[0]), self.lines[3].YAtX(point[0])) <= point[1] <= \
+                        greater(self.lines[1].YAtX(point[0]), self.lines[3].YAtX(point[0])):
                     return 1
 
             for point in self.points:
-                if lesser(rectangle.lines[0].YAtX(point[0]), rectangle.lines[2].YAtX(point[0])) <= point[1] <= greater(rectangle.lines[0].YAtX(point[0]), rectangle.lines[2].YAtX(point[0])) \
-                        and lesser(rectangle.lines[1].YAtX(point[0]), rectangle.lines[3].YAtX(point[0])) <= point[1] <= greater(rectangle.lines[1].YAtX(point[0]), rectangle.lines[3].YAtX(point[0])):
+                if lesser(rectangle.lines[0].YAtX(point[0]), rectangle.lines[2].YAtX(point[0])) <= point[1] <= \
+                        greater(rectangle.lines[0].YAtX(point[0]), rectangle.lines[2].YAtX(point[0])) \
+                        and lesser(rectangle.lines[1].YAtX(point[0]), rectangle.lines[3].YAtX(point[0])) <= point[1] \
+                        <= greater(rectangle.lines[1].YAtX(point[0]), rectangle.lines[3].YAtX(point[0])):
                     return 1
 
             for rectLine in rectangle.lines:
