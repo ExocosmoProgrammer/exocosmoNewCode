5856227ed2b38011cc488c77650b733fc27b4e95
ExocosmoProgrammer <gooberpea1024@gmail.com>
2023-12-16 14:41:05 -0800

    Update worldFile.py

--- worldFile.py
+++ worldFile.py
@@ -2,11 +2,23 @@ from world import world
 from definitions import saveWithPickle, loadWithPickle
 
 rooms = world()
-rooms.makeRooms(1, 'desert', (0, 6, -1), {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0})
-rooms.makeRooms(5, 'desert', rooms.desertCaveDepthTwoEntranceCoord, {0: 0})
-rooms.makeRooms(5, 'desert', rooms.desertCaveDepthThreeEntranceCoord, {0: 0})
+rooms.makeRooms(100, 'desert', (0, 6, -1), {0: 7, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2})
+rooms.makeRooms(75, 'desert', rooms.desertCaveDepthTwoEntranceCoord, {0: 0})
+rooms.makeRooms(75, 'desert', rooms.desertCaveDepthThreeEntranceCoord, {0: 0})
 rooms.addDoors()
 
 
 def load(file):
     rooms = loadWithPickle(f'worldSave{file}.pickle')
+
+
+def reset():
+    global rooms
+
+    rooms = world()
+    rooms.makeRooms(100, 'desert', (0, 6, -1), {0: 7, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2})
+    rooms.makeRooms(75, 'desert', rooms.desertCaveDepthTwoEntranceCoord, {0: 0})
+    rooms.makeRooms(75, 'desert', rooms.desertCaveDepthThreeEntranceCoord, {0: 0})
+    rooms.addDoors()
+
+    return rooms
