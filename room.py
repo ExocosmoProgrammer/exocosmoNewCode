1a8bc3b3ad78cdf4ab5d05d30733b909725b5190
ExocosmoProgrammer <gooberpea1024@gmail.com>
2023-12-16 14:36:38 -0800

    Update room.py

--- room.py
+++ room.py
@@ -18,12 +18,14 @@ class room:
         self.damagingTraps = []
         self.waves = []
         self.teleporters = []
-        self.locks = 1
+        self.locks = 0
         self.wave = -1
         self.leftXBoundary = 0
         self.rightXBoundary = width
         self.difficulty = -1
         roomActions = {('desert', -3): self.actAsDesertCaveDepthThreeRoom}
+        self.environmentObjects = []
+        self.temporaryAnimations = []
 
         try:
             self.action = roomActions[(self.biome, self.coordinate[2])]
@@ -42,24 +44,26 @@ class room:
         elif biome == 'desert' and coordinate[2] == -1:
             self.background = 'topDesertCaveBackground.bmp'
             self.yBoundaries = 0
-            self.standardRoomMaxDifficulty = random.randint(5, 15)
-            self.foeDifficulties = {'desertCaveJellyfish': 1, 'desertCaveLargeFly': 2}
+            self.standardRoomMaxDifficulty = random.choice([3, 3, 3, 4, 4, 4, 5, 5, 6, 7])
+            self.foeDifficulties = {'desertCaveJellyfish': (1, 33), 'desertCaveLargeFly': (1, 33),
+                                    'desertCaveSpider': (5, 10)}
 
         elif biome == 'desert' and coordinate[2] == -2:
             self.background = 'temporaryDesertCave.bmp'
             self.yBoundaries = 0
             self.standardRoomMaxDifficulty = random.randint(7, 8)
-            self.foeDifficulties = {'desertCaveSummoner': 4}
+            self.foeDifficulties = {'desertCaveSummoner': (4, 25)}
 
         elif biome == 'desert' and coordinate[2] == -3:
             self.background = 'temporaryBottomDesertCave.bmp'
             self.yBoundaries = 0
             self.standardRoomMaxDifficulty = 20
-            self.foeDifficulties = {'antlionLarva': 1, 'desertCaveSummoner': 4}
+            self.foeDifficulties = {'antlionLarva': (1, 50), 'desertCaveSummoner': (4, 25)}
             self.spawnCooldown = 100
 
         elif biome == 'ship':
             self.yBoundaries = 84 * height / 900
+            self.difficulty = -2
 
         for stat in list(extra.keys()):
             exec(f'self.{stat} = extra[stat]')
@@ -74,6 +78,9 @@ class room:
                 for foe in wave:
                     foe.yBoundary = (165 - foe.place.height) * height / 900
 
+        for enemy in self.foes:
+            enemy.yBoundary = self.yBoundaries
+
     def addFoes(self, difficulty):
         self.difficulty = difficulty
 
@@ -81,27 +88,26 @@ class room:
             self.mapMarker = 'difficultyFiveMapImage.png'
 
         if self.biome == 'desert' and self.coordinate[2] == -1:
-            self.foes = [foe('antlionLarva', random.randint(int(self.leftXBoundary), int(self.rightXBoundary)),
+            self.foes = [foe('desertCaveJellyfish', random.randint(int(self.leftXBoundary), int(self.rightXBoundary)),
                              random.randint(int(self.yBoundaries), height),
-                             self.coordinate) for i in range(30)]
+                             self.coordinate) for i in range(5)]
 
             if difficulty >= 1:
-                self.foes += [foe('desertCaveSummoner', random.randint(int(width / 16), int(width * 15 / 16)),
-                                  random.randint(int(height / 16), int(height * 15 / 16)), self.coordinate)]
+                self.foes += [foe('desertCaveLargeFly', random.randint(int(width / 16), int(width * 15 / 16)),
+                                  random.randint(int(height / 16), int(height * 5 / 16)), self.coordinate) for
+                              i in range(difficulty - 1)]
 
-            if difficulty >= 3:
-                self.foes += [foe('desertCaveSummoner', random.randint(int(width / 16), int(width * 15 / 16)),
-                                  random.randint(int(height / 16), int(height * 15 / 16)),
-                                  self.coordinate) for i in range(difficulty - 2)]
-
-            if difficulty in [0, 3, 4, 5]:
-                self.foes += [foe('desertCaveSpittingGrub',
+            if difficulty >= 4:
+                self.foes += [foe('desertCaveSpider',
                                   random.randint(int(self.leftXBoundary), int(self.rightXBoundary)),
                                   random.randint(int(self.yBoundaries), height),
-                                  self.coordinate) for i in range(5)]
+                                  self.coordinate)]
+
+        for enemy in self.foes:
+            enemy.yBoundary = self.yBoundaries
 
     def spawnFoesFromRoomSwitch(self):
-        if random.randint(1, 6) == 1:
+        """if random.randint(1, 6) == 1:
             currentCumulativeDifficulty = 0
 
             for enemy in self.foes:
@@ -121,7 +127,25 @@ class room:
                                       self.coordinate))
 
             except IndexError:
+                pass"""
+        currentCumulativeDifficulty = 0
+
+        for enemy in self.foes:
+            try:
+                currentCumulativeDifficulty += self.foeDifficulties[enemy.type][0]
+
+            except KeyError:
                 pass
+
+        for enemy in self.foeDifficulties.keys():
+            if random.randint(1, 100) <= self.foeDifficulties[enemy][1] and self.foeDifficulties[enemy][0] <= \
+                    self.standardRoomMaxDifficulty - currentCumulativeDifficulty:
+                self.foes.append(foe(enemy, random.randint(int(width / 16), int(width * 15 / 16)),
+                                     random.randint(int(height / 16), int(height * 15 / 16)),
+                                     self.coordinate))
+
+        for enemy in self.foes:
+            enemy.yBoundary = self.yBoundaries
 
     def actAsDesertCaveDepthThreeRoom(self):
         self.spawnCooldown -= GAMESPEED
@@ -130,3 +154,4 @@ class room:
             self.foes.append(foe('antlionLarva', random.randint(int(width / 16), int(width * 15 / 16)),
                                  random.randint(int(height / 16), int(height * 15 / 16)), self.coordinate))
             self.spawnCooldown = 100
+
