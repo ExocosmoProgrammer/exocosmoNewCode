aadedfac1ab94528e1044da00c83e05a2332f358
ExocosmoProgrammer <gooberpea1024@gmail.com>
2024-05-11 13:01:00 -0700

    Update worldFile.py

--- worldFile.py
+++ worldFile.py
@@ -2,7 +2,7 @@ from world import world
 from definitions import saveWithPickle, loadWithPickle
 
 rooms = world()
-rooms.makeRooms(100, 'desert', (0, 6, -1), {0: 7, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2})
+rooms.makeRooms(100, 'desert', (0, 6, -1), {-2: 50, 0: 3, 1: 3, 2: 3, 3: 3, 4: 2, 5: 2})
 rooms.makeRooms(75, 'desert', rooms.desertCaveDepthTwoEntranceCoord, {0: 0})
 rooms.makeRooms(75, 'desert', rooms.desertCaveDepthThreeEntranceCoord, {0: 0})
 rooms.addDoors()
@@ -16,9 +16,9 @@ def reset():
     global rooms
 
     rooms = world()
-    rooms.makeRooms(100, 'desert', (0, 6, -1), {0: 7, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2})
+    rooms.makeRooms(100, 'desert', (0, 6, -1),
+                    {-2: 70, 0: 3, 1: 3, 2: 3, 3: 3, 4: 2, 5: 2})
     rooms.makeRooms(75, 'desert', rooms.desertCaveDepthTwoEntranceCoord, {0: 0})
     rooms.makeRooms(75, 'desert', rooms.desertCaveDepthThreeEntranceCoord, {0: 0})
     rooms.addDoors()
-
     return rooms
