2893f30d4e85b05bf81b33713beb24455bfced85
ExocosmoProgrammer <gooberpea1024@gmail.com>
2024-05-29 13:09:14 -0700

    Update plainSprites.py

--- plainSprites.py
+++ plainSprites.py
@@ -1,7 +1,10 @@
 from variables import IMAGES
+from rects import rect
 
 
 class plainSprite:
     def __init__(self, sprite, centerx, centery):
+        """A plainSprite object is just something to draw."""
         self.sprite = sprite
         self.place = IMAGES[sprite].get_rect(center=(centerx, centery))
+        self.hitbox = rect(self.place)
