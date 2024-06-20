c45169854abe479e0b7bf6113baafd19074d3da8
ExocosmoProgrammer <gooberpea1024@gmail.com>
2024-06-20 11:15:46 -0400

    Update button.py

--- button.py
+++ button.py
@@ -1,9 +1,20 @@
 from variables import IMAGES
 from rects import rect
+from definitions import draw, checkMouseCollision
 
 
 class button:
-    def __init__(self, sprite, x, y):
+    def __init__(self, sprite, x, y, spriteWhenTouchingMouse=None):
+        self.spriteWhenTouchingMouse = spriteWhenTouchingMouse
+
+        if spriteWhenTouchingMouse is None:
+            self.spriteWhenTouchingMouse = sprite
+
         self.sprite = sprite
-        self.place = IMAGES[self.sprite].get_rect(center=(x, y))
+        self.normalSprite = sprite
+        self.place = IMAGES[sprite].get_rect(center=(x, y))
         self.hitbox = rect(self.place)
+
+    def draw(self):
+        self.sprite = self.spriteWhenTouchingMouse if checkMouseCollision(self.hitbox) else self.normalSprite
+        draw(self)
