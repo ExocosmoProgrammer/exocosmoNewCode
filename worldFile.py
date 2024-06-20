a82539c4dba32a6bc51ffd4205f14d3d2fcbb7fe
ExocosmoProgrammer <gooberpea1024@gmail.com>
2024-06-20 11:28:26 -0400

    Update worldFile.py

--- worldFile.py
+++ worldFile.py
@@ -1,15 +1,4 @@
 from world import world
-from definitions import saveWithPickle, loadWithPickle
-
-rooms = world()
-rooms.makeRooms(100, 'desert', (0, 6, -1), {-2: 50, 0: 3, 1: 3, 2: 3, 3: 3, 4: 2, 5: 2})
-rooms.makeRooms(75, 'desert', rooms.desertCaveDepthTwoEntranceCoord, {0: 0})
-rooms.makeRooms(75, 'desert', rooms.desertCaveDepthThreeEntranceCoord, {0: 0})
-rooms.addDoors()
-
-
-def load(file):
-    rooms = loadWithPickle(f'worldSave{file}.pickle')
 
 
 def reset():
