fb6c4535b7f2ce57bd9719eb7f528f00e268e602
ExocosmoProgrammer <gooberpea1024@gmail.com>
2024-07-16 08:57:35 -0400

    Update button.py

--- button.py
+++ button.py
@@ -5,16 +5,29 @@ from definitions import draw, checkMouseCollision
 
 class button:
     def __init__(self, sprite, x, y, spriteWhenTouchingMouse=None):
-        self.spriteWhenTouchingMouse = spriteWhenTouchingMouse
+        # self.spriteWhenTouching mouse should default to being self.sprite.
+        # self should use a method to draw based on if self has a different sprite when touching the mouse.
 
-        if spriteWhenTouchingMouse is None:
-            self.spriteWhenTouchingMouse = sprite
+        if spriteWhenTouchingMouse is not None:
+            self.spriteWhenTouchingMouse = spriteWhenTouchingMouse
+            self.drawingMethod = self.drawAsButtonWithSpriteWhenTouchingMouse
+            self.normalSprite = sprite
 
+        else:
+            self.drawingMethod = self.draw
+
+        # Define the other attributes.
         self.sprite = sprite
-        self.normalSprite = sprite
         self.place = IMAGES[sprite].get_rect(center=(x, y))
         self.hitbox = rect(self.place)
 
-    def draw(self):
+    def drawAsButtonWithSpriteWhenTouchingMouse(self):
+        """Adjust self's sprite based on whether self is touching the mouse and draw self."""
+
+        # Adjust self.sprite based on if the mouse is touching self. Then draw self.
         self.sprite = self.spriteWhenTouchingMouse if checkMouseCollision(self.hitbox) else self.normalSprite
+        draw(self)
+
+    def draw(self):
+        """Draw self."""
         draw(self)
