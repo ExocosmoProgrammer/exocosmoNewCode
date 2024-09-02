1a7c6869a19e2f0721e47ba48998953a2ebe35e5
ExocosmoProgrammer <gooberpea1024@gmail.com>
2024-09-02 13:39:13 -0400

    Update proFile.py

--- proFile.py
+++ proFile.py
@@ -1,14 +1,10 @@
+# This file should not be used anymore.
+# TODO Delete this file.
+
 from pro import player
-from definitions import loadWithPickle
 
 pro = player()
 
 
-def load(file):
-    pro = loadWithPickle(f'playerSave{file}.pickle')
-
-
 def reset():
-    global pro
-    pro = player()
-    return pro
+    return player()
