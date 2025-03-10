67806da883da357e1e67a7f526ea43fec4f6f3e3
ExocosmoProgrammer <gooberpea1024@gmail.com>
2025-03-10 13:57:02 -0400

    Update word.py

--- word.py
+++ word.py
@@ -4,22 +4,28 @@ from plainSprites import plainSprite
 
 
 class word:
-    def __init__(self, left, y, text, font):
+    def __init__(self, left, y, text, font, extraHorizontalSpacing=width / 320):
+        """wordObject(left, y, text, font).draw() will draw the requested text to the display with the requested
+        leftmost point and with a vertical center at y."""
+        # self.currentRight will always determine the leftmost point of the next letter to be added to self.
         self.currentRight = left
         self.characters = [font + i for i in text]
         self.drawnLetters = []
-        text = text.upper()
 
         for i in self.characters:
-            self.currentRight += width / 320
+            self.currentRight += extraHorizontalSpacing
 
             if i[-1] != ' ':
                 characterWidth = IMAGES[f'{i}.png'].get_width()
                 centerx = self.currentRight + characterWidth / 2
                 self.currentRight += characterWidth
 
-                if i[-1] == ',' or i[-1] == '.':
+                if i[-1] in ['.', ',']:
+                    # Commas and periods will have lower centers than other characters.
                     centery = y + 11 * height / 1800
+
+                elif i[-1] == "'":
+                    centery = y - 11 * height / 1800
 
                 else:
                     centery = y
@@ -27,10 +33,18 @@ class word:
                 self.drawnLetters.append(plainSprite(f'{i}.png', centerx, centery))
 
             else:
-                self.currentRight += width / 320
+                self.currentRight += extraHorizontalSpacing
 
         self.width = self.currentRight - left
 
-    def draw(self):
+    def move(self, x, y):
+        """Move self."""
+
         for i in self.drawnLetters:
-            draw(i)
+            i.place.x += x
+            i.place.y += y
+            i.hitbox.move(x, y)
+
+    def draw(self, offset=(0, 0)):
+        for i in self.drawnLetters:
+            draw(i, offset=offset)
